2caf201ab0454aa4469a0a91246d32d7db6c8c44
hyoshida <hyoshida@git.com>
2018-07-15 17:11:11 -0700

    Initial commit

---
+++ app/src/main/java/io/github/hyoshida123/yomiganacamera/MainActivity.java
@@ -0,0 +1,45 @@
+package io.github.hyoshida123.yomiganacamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
+    static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageButton.setImageBitmap(bitmap);
+        }
+    }
+}
+
+
